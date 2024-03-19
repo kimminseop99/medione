1768ad2e0a7a795484989ed18160a5d00cab3cae
kimminseop99 <minseopkim99@gmail.com>
2024-03-20 00:23:01 +0900

    스캐너 받기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("안녕");
+        System.out.println("== 프로그램 시작 ==");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("명령어) ");
+        String cmd = sc.nextLine();
+        System.out.printf("입력된 명령어 : %s\n", cmd);
+
+        System.out.printf("명령어) ");
+        int num = sc.nextInt();
+        sc.nextLine();
+        System.out.printf("입력된 명령어 : %d\n", num);
+
+        sc.close();
+        System.out.println("== 프로그램 끝 ==");
     }
 }
