ed8bb3f0f1269ca16937e408ae352126e9777655
kimminseop99 <minseopkim99@gmail.com>
2024-03-25 16:27:40 +0900

    한 번 사용된게 시ë¬ 번호가 재사용되지 않도록 lastId 도ì

--- src/main/java/org/example/dao/ArticleDao.java
+++ src/main/java/org/example/dao/ArticleDao.java
@@ -5,10 +5,15 @@ import org.example.dto.Article;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArticleDao {
+public class ArticleDao extends Dao{
     public List<Article> articls;
 
     public ArticleDao(){
         articls = new ArrayList<>();
+    }
+
+    public void add(Article article) {
+        articls.add(article);
+        lastId++;
     }
 }
